0a82dd99d3fc838da52ab578351b15cfa24462c6
Pedram Behradkian <behradkian@outlook.com>
2024-05-22 16:01:26 +0330

    [revision] c5f6bea1e68634094f0440a1ce37782b1a481f54 add xml converter

--- src/main/java/ir/baarmaan/utility/convertor/XmlConvertor.java
+++ src/main/java/ir/baarmaan/utility/convertor/XmlConvertor.java
@@ -2,17 +2,42 @@ package ir.baarmaan.utility.convertor;
 
 import org.json.JSONObject;
 import org.json.XML;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class XmlConvertor {
 
     private XmlConvertor() {
     }
 
-    public static JSONObject convertXmlToJsonObject(String xml){
+    public static Document convertXmlToDocument(String xml) {
+
+        Document document;
+        try {
+            byte[] bytes = GeneralConvertor.convertStringToByteArray(xml, StandardCharsets.UTF_8);
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            document = documentBuilder.parse(new ByteArrayInputStream(bytes));
+            document.normalizeDocument();
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            document = null;
+        }
+        return document;
+    }
+
+    public static JSONObject convertXmlToJsonObject(String xml) {
         return XML.toJSONObject(xml);
     }
 
-    public static String convertXmlToJsonString(String xml){
+    public static String convertXmlToJsonString(String xml) {
         return convertXmlToJsonObject(xml).toString(2);
     }
+
 }
