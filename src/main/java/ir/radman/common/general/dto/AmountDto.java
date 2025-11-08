3a4a26c76ed0525ecd30c5de8cf398433e9d40da
Pedram Behradkian <behradkian@outlook.com>
2025-11-08 23:10:28 +0330

    [feature] amountDto

--- src/main/java/ir/radman/common/general/dto/AmountDto.java
+++ src/main/java/ir/radman/common/general/dto/AmountDto.java
@@ -10,9 +10,7 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Amount {
-
+public class AmountDto {
     private BigDecimal amount;
     private Currency currency;
-
 }
